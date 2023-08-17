2933df140f523734eed37068926d374231a6fd39
reidyllalima <reidyllalima@hotmail.com>
2023-08-17 20:38:28 -0300

    object

---
+++ exercicios-js/object.js
@@ -0,0 +1,19 @@
+const prod1 = {}
+prod1.nome = 'Iphone 13'
+prod1.preco = 4299.99
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço 
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    obj: {
+        nome: 1,
+        obj: {
+            teste: 2
+        }
+    }
+}
+
+console.log(prod2)
