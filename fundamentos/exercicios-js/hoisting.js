ba11155eb725aa26b40a93e1973f59bada48d85c
reidyllalima <reidyllalima@hotmail.com>
2023-10-05 21:32:09 -0300

    hoistinh

---
+++ fundamentos/exercicios-js/hoisting.js
@@ -0,0 +1,12 @@
+console.log('a=', a)
+
+var a = 2
+console.log('a =', a)
+
+// Joga a variável p cime, por ex: var a 
+
+// Com let, não existe o hoisting
+console.log('b=', b)
+
+let b = 2
+console.log('b =', b)
